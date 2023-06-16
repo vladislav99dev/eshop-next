48470740534e55a20ad6b865d46a4ab51eee768a
vladislav99dev <vladislavcho99@abv.bg>
2023-06-16 15:33:55 +0300

    add check icon

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -31,3 +31,20 @@ export const BigCloseIcon = () => (
     />
   </svg>
 );
+
+export const BigSuccessIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="white"
+    className="w-32 h-32"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+);
